a5e32f7d3f90b85e54e2a21c4b0e1c9e14c2495d
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2019-03-18 17:50:26 -0600

    Se añadio la prueba en .txt

--- build_lexico.sh
+++ build_lexico.sh
@@ -7,4 +7,6 @@ rm -rf .analizador_lexico.l.swp
 # Construimos
 lex -o analizador_lexico.yy.c analizador_lexico.l
 gcc analizador_lexico.yy.c -o analizador_lexico.o
-./analizador_lexico.o prueba.c
+#./analizador_lexico.o prueba.c
+./analizador_lexico.o prueba.txt
+
